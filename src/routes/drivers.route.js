6add44600074b72de184b63d6afb57ad0ca72824
Nicholas Chung <iamnicholaschung@gmail.com>
2021-03-25 01:36:36 +0800

    Remove request and use axios

--- src/routes/drivers.route.js
+++ src/routes/drivers.route.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const request = require("request");
+const axios = require("axios");
 
 router.get("/", async (req, res) => {
   let count = req.query.count;
   let lat = req.query.latitude;
   let lng = req.query.longitude;
   const url = `https://qa-interview-test.splytech.dev/api/drivers?latitude=${lat}&longitude=${lng}&count=${count}`;
-  request(url).pipe(res);
+  axios.get(url).then((response) => {
+    res.send(response.data);
+  });
 });
 
 module.exports = router;
